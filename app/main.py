746fb2de3e6d1c19be7a2e24c54c013568615e9f
maladesov <m.zaharenkov@mail.ru>
2025-04-13 18:40:42 +0300

    📝 Документирование существующих эндпоинтов в Swagger

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 from app.db import init_db
 from app.views import main_router
 
-app = FastAPI(title="DSTU Diploma | AuthService", docs_url="/swagger")
+app = FastAPI(title="DSTU Diploma | UserService", docs_url="/swagger")
 init_db(app)
 app.include_router(main_router)
--- app/views/user/__init__.py
+++ app/views/user/__init__.py
@@ -13,7 +13,11 @@ from fastapi import APIRouter, Depends, Query
 router = APIRouter(prefix="")
 
 
-@router.post("/", response_model=RegisteredUserDto)
+@router.post(
+    "/",
+    response_model=RegisteredUserDto,
+    description="Создает нового пользователя",
+)
 async def create(
     user_dto: CreateUserDto,
     user_controller: UserController = Depends(get_user_controller),
@@ -21,7 +25,11 @@ async def create(
     return await user_controller.create(user_dto.password, user_dto)
 
 
-@router.post("/login", response_model=RegisteredUserDto)
+@router.post(
+    "/login",
+    response_model=RegisteredUserDto,
+    description="Логинит пользователя, обновляет ревизию рефреш-токена и возвращает пару токенов",
+)
 async def login(
     input: OAuth2PasswordRequestForm = Depends(),
     user_controller: UserController = Depends(get_user_controller),
@@ -29,7 +37,11 @@ async def login(
     return await user_controller.login(input.username, input.password)
 
 
-@router.patch("/", response_model=FullUserDto)
+@router.patch(
+    "/",
+    response_model=FullUserDto,
+    description="Обновляет некоторые данные о пользователе в СУБД",
+)
 async def update(
     update_dto: OptionalFullUserData,
     user_controller: UserController = Depends(get_user_controller),
@@ -37,7 +49,7 @@ async def update(
     return await user_controller.update_info(update_dto)
 
 
-@router.delete("/")
+@router.delete("/", description="Удаляет пользователя из СУБД")
 async def delete(
     user_id: int = Query(..., ge=0),
     user_controller: UserController = Depends(get_user_controller),
@@ -45,14 +57,21 @@ async def delete(
     return await user_controller.delete(user_id)
 
 
-@router.get("/")
+@router.get(
+    "/",
+    description="Возвращает список всех зарегистрированных пользователей (с минимальным набором данных)",
+)
 async def get_all(
     user_controller: UserController = Depends(get_user_controller),
 ):
     return await user_controller.get_all()
 
 
-@router.get("/me", response_model=FullUserDto)
+@router.get(
+    "/me",
+    response_model=FullUserDto,
+    description="Возвращает полные данные о текущем залогиненном пользователе",
+)
 async def get_me(
     user_dto: AccessJWTPayloadDto = Depends(get_user_dto),
     user_controller: UserController = Depends(get_user_controller),
