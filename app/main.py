7080065171fde1b7ac9a06ebd9e494256aa80887
radon-dro <m.zaharenkov@dro.red>
2025-04-19 19:00:33 +0300

    💩 CORS

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
+from fastapi.middleware.cors import CORSMiddleware
+from app.views import main_router
 from fastapi import FastAPI
 from app.db import init_db
-from app.views import main_router
 from os import environ
 
+ROOT_PATH = environ.get("ROOT_PATH", "/")
 
-ROOT_PATH = environ.get('ROOT_PATH', '/')
-
-app = FastAPI(title="DSTU Diploma | UserService", docs_url="/swagger", root_path=ROOT_PATH)
+app = FastAPI(
+    title="DSTU Diploma | UserService", docs_url="/swagger", root_path=ROOT_PATH
+)
 init_db(app)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(main_router)
