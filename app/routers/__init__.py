63d41da33dc016ee32c82699e20c444f94b9ecc4
maladesov <m.zaharenkov@mail.ru>
2025-05-18 17:28:20 +0300

    ✨ Забыл заинклудить прокси

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .root import router as root_router
 from .admin import router as admin_router
+from .proxy import router as proxy_router
 from .internal import router as internal_router
 
 main_router = APIRouter()
 main_router.include_router(root_router)
 main_router.include_router(admin_router)
 main_router.include_router(internal_router)
+main_router.include_router(proxy_router)
